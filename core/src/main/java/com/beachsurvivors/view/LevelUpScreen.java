74baea221cba0b9a4ef7213ba20654023eb8da41
Bjornheimer <christoffer.bjornheimer@gmail.com>
2025-04-28 23:19:46 +0200

    Lite assets
    Death Screen och How To Play

--- core/src/main/java/com/beachsurvivors/view/LevelUpScreen.java
+++ core/src/main/java/com/beachsurvivors/view/LevelUpScreen.java
@@ -36,10 +36,8 @@ public class LevelUpScreen implements Screen {
 //        table.setDebug(true);
 
         table.setBackground(skin.getDrawable("textfield"));
-        int level =player.getLevel();
-        level = level +1;
 
-        Label title = new Label("You've reached level "+ level + "!", skin);
+        Label title = new Label("You've reached level "+ player.getLevel() + "!", skin);
         TextButton upgrade1 = new TextButton("+1 BOOMERANG", skin);
         TextButton upgrade2 = new TextButton("25% INCREASED MOVEMENT SPEED", skin);
         TextButton upgrade3 = new TextButton("+10% CRIT CHANCE", skin);
