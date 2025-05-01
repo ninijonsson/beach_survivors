f54d8ba92f23dcdbcaea84de3c9012b908ce51cf
Nicole Jonsson <143434132+ninijonsson@users.noreply.github.com>
2025-05-01 22:28:01 +0200

    Tagit bort filer som inte används

--- core/src/main/java/com/beachsurvivors/view/PauseScreen.java
+++ core/src/main/java/com/beachsurvivors/view/PauseScreen.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 import javax.swing.event.ChangeEvent;
@@ -31,18 +32,29 @@ public class PauseScreen implements Screen {
     private void buildUI() {
         Window pauseMenu = new Window("", skin, "default");
         pauseMenu.setSize(1200, 972); // Bildens dimensioner
+        //pauseMenu.setDebug(true); // Rutnät för debugging
 
         // Centrera pausmenyn
         pauseMenu.setPosition(game.getScreenWidth() / 5f,
             stage.getHeight() / 2f);
 
+        ImageButton soundOnButton = new ImageButton(skin, "sound_on");
+        ImageButton soundOffButton = new ImageButton(skin, "sound_off");
+        ImageButton restartButton = new ImageButton(skin, "restart");
+
         ImageButton resumeButton = new ImageButton(skin, "resume");
         ImageButton helpButton = new ImageButton(skin, "help");
         ImageButton exitButton = new ImageButton(skin, "exit");
 
-        pauseMenu.add(resumeButton).width(256).height(64).pad(10).padTop(420).row();
-        pauseMenu.add(helpButton).width(256).height(64).pad(10).row();
-        pauseMenu.add(exitButton).width(256).height(64).pad(10).row();
+        // Runda knappar
+        pauseMenu.add(soundOnButton).size(96, 96).left().padTop(260);
+        pauseMenu.add(restartButton).size(96, 96).padTop(260);
+        pauseMenu.add().row(); // Ny rad
+
+        // Avlånga knappar
+        pauseMenu.add(resumeButton).size(333, 83).pad(10).padTop(70).padLeft(100).row();
+        pauseMenu.add(helpButton).size(333, 83).pad(10).padLeft(100).row();
+        pauseMenu.add(exitButton).size(333, 83).pad(10).padLeft(100).row();
 
         stage.addActor(pauseMenu);
 
